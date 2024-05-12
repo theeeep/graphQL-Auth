6b0ab59c3967076328a429155e226137dce2c683
theeeep <ideepakrabindra@gmail.com>
2024-05-12 19:36:09 +0530

    - feat: Add user GraphQL module - chore: Update index.ts to include user GraphQL module - chore: Update tsconfig.json

---
+++ src/graphql/user/resolvers.ts
@@ -0,0 +1,9 @@
+const queries = {};
+
+const mutations = {
+  createUser: async (_: any, {}: {}) => {
+    return "Yo Yo";
+  },
+};
+
+export const resolvers = { queries, mutations };
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import express from "express";
 require("dotenv").config();
 import { expressMiddleware } from "@apollo/server/express4";
-import createApolloGraphqlServer from "./graphql";
+import { ApolloServer } from "@apollo/server";
+import { User } from "graphql/user";
 
 async function init() {
   const app = express();
@@ -12,12 +13,32 @@ async function init() {
   app.get("/", (req, res) => {
     res.json({ message: "Server is up and running" });
   });
+  const gqlServer = new ApolloServer({
+    typeDefs: `
+           
+            type Query {
+               hello:String
+            }
 
-  app.use("/graphql", expressMiddleware(await createApolloGraphqlServer()));
-
-  app.listen(PORT, () => {
-    console.log(`Server🚀 listening on ${PORT}`);
+            type Mutation {
+              ${User.mutations}
+            }
+        `,
+    resolvers: {
+      Query: {
+        ...User.resolvers.queries,
+      },
+      Mutation: {
+        ...User.resolvers.mutations,
+      },
+    },
   });
+
+  // Start the gql server
+  await gqlServer.start();
+
+  app.use("/", expressMiddleware(gqlServer));
+  app.listen(PORT, () => console.log(`Server started at PORT:${PORT}`));
 }
 
 init();
